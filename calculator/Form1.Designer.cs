13ca12770f81c4f3e0e9b1eaaf81230abe7ead55
vaso kapanadze <vasokapanadzemicrosoft@gmail.com>
2023-12-02 14:04:26 +0400

    Add project files.

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.n1 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.wertili = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.washla = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.gamravleba = new System.Windows.Forms.Button();
+            this.pliusi = new System.Windows.Forms.Button();
+            this.toloba = new System.Windows.Forms.Button();
+            this.minusi = new System.Windows.Forms.Button();
+            this.gayofa = new System.Windows.Forms.Button();
+            this.gamotvla = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // n1
+            // 
+            this.n1.BackColor = System.Drawing.Color.White;
+            this.n1.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n1.ForeColor = System.Drawing.Color.Red;
+            this.n1.Location = new System.Drawing.Point(10, 116);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(100, 100);
+            this.n1.TabIndex = 0;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = false;
+            this.n1.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // n2
+            // 
+            this.n2.BackColor = System.Drawing.Color.White;
+            this.n2.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n2.ForeColor = System.Drawing.Color.Red;
+            this.n2.Location = new System.Drawing.Point(116, 116);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(100, 100);
+            this.n2.TabIndex = 0;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = false;
+            this.n2.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // n3
+            // 
+            this.n3.BackColor = System.Drawing.Color.White;
+            this.n3.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n3.ForeColor = System.Drawing.Color.Red;
+            this.n3.Location = new System.Drawing.Point(222, 116);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(100, 100);
+            this.n3.TabIndex = 0;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = false;
+            // 
+            // n4
+            // 
+            this.n4.BackColor = System.Drawing.Color.White;
+            this.n4.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n4.ForeColor = System.Drawing.Color.Red;
+            this.n4.Location = new System.Drawing.Point(10, 222);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(100, 100);
+            this.n4.TabIndex = 0;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = false;
+            // 
+            // n5
+            // 
+            this.n5.BackColor = System.Drawing.Color.White;
+            this.n5.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n5.ForeColor = System.Drawing.Color.Red;
+            this.n5.Location = new System.Drawing.Point(116, 222);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(100, 100);
+            this.n5.TabIndex = 0;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = false;
+            // 
+            // n6
+            // 
+            this.n6.BackColor = System.Drawing.Color.White;
+            this.n6.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n6.ForeColor = System.Drawing.Color.Red;
+            this.n6.Location = new System.Drawing.Point(222, 222);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(100, 100);
+            this.n6.TabIndex = 0;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = false;
+            // 
+            // wertili
+            // 
+            this.wertili.BackColor = System.Drawing.Color.White;
+            this.wertili.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wertili.ForeColor = System.Drawing.Color.Red;
+            this.wertili.Location = new System.Drawing.Point(10, 434);
+            this.wertili.Name = "wertili";
+            this.wertili.Size = new System.Drawing.Size(100, 100);
+            this.wertili.TabIndex = 0;
+            this.wertili.Text = ".";
+            this.wertili.UseVisualStyleBackColor = false;
+            // 
+            // n7
+            // 
+            this.n7.BackColor = System.Drawing.Color.White;
+            this.n7.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n7.ForeColor = System.Drawing.Color.Red;
+            this.n7.Location = new System.Drawing.Point(10, 328);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(100, 100);
+            this.n7.TabIndex = 0;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = false;
+            // 
+            // n9
+            // 
+            this.n9.BackColor = System.Drawing.Color.White;
+            this.n9.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n9.ForeColor = System.Drawing.Color.Red;
+            this.n9.Location = new System.Drawing.Point(222, 328);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(100, 100);
+            this.n9.TabIndex = 0;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = false;
+            // 
+            // n8
+            // 
+            this.n8.BackColor = System.Drawing.Color.White;
+            this.n8.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n8.ForeColor = System.Drawing.Color.Red;
+            this.n8.Location = new System.Drawing.Point(116, 328);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(100, 100);
+            this.n8.TabIndex = 0;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = false;
+            // 
+            // washla
+            // 
+            this.washla.BackColor = System.Drawing.Color.White;
+            this.washla.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.washla.ForeColor = System.Drawing.Color.Blue;
+            this.washla.Location = new System.Drawing.Point(222, 434);
+            this.washla.Name = "washla";
+            this.washla.Size = new System.Drawing.Size(100, 100);
+            this.washla.TabIndex = 0;
+            this.washla.Text = "C";
+            this.washla.UseVisualStyleBackColor = false;
+            // 
+            // n0
+            // 
+            this.n0.BackColor = System.Drawing.Color.White;
+            this.n0.Font = new System.Drawing.Font("Monotype Corsiva", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.n0.ForeColor = System.Drawing.Color.Red;
+            this.n0.Location = new System.Drawing.Point(116, 434);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(100, 100);
+            this.n0.TabIndex = 0;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = false;
+            // 
+            // gamravleba
+            // 
+            this.gamravleba.BackColor = System.Drawing.Color.White;
+            this.gamravleba.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gamravleba.ForeColor = System.Drawing.Color.Black;
+            this.gamravleba.Location = new System.Drawing.Point(328, 116);
+            this.gamravleba.Name = "gamravleba";
+            this.gamravleba.Size = new System.Drawing.Size(100, 100);
+            this.gamravleba.TabIndex = 0;
+            this.gamravleba.Text = "×";
+            this.gamravleba.UseVisualStyleBackColor = false;
+            // 
+            // pliusi
+            // 
+            this.pliusi.BackColor = System.Drawing.Color.White;
+            this.pliusi.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pliusi.ForeColor = System.Drawing.Color.Black;
+            this.pliusi.Location = new System.Drawing.Point(328, 328);
+            this.pliusi.Name = "pliusi";
+            this.pliusi.Size = new System.Drawing.Size(100, 100);
+            this.pliusi.TabIndex = 0;
+            this.pliusi.Text = "+";
+            this.pliusi.UseVisualStyleBackColor = false;
+            // 
+            // toloba
+            // 
+            this.toloba.BackColor = System.Drawing.Color.White;
+            this.toloba.Font = new System.Drawing.Font("Monotype Corsiva", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toloba.ForeColor = System.Drawing.Color.Black;
+            this.toloba.Location = new System.Drawing.Point(328, 434);
+            this.toloba.Name = "toloba";
+            this.toloba.Size = new System.Drawing.Size(100, 100);
+            this.toloba.TabIndex = 0;
+            this.toloba.Text = "=";
+            this.toloba.UseVisualStyleBackColor = false;
+            // 
+            // minusi
+            // 
+            this.minusi.BackColor = System.Drawing.Color.White;
+            this.minusi.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minusi.ForeColor = System.Drawing.Color.Black;
+            this.minusi.Location = new System.Drawing.Point(328, 222);
+            this.minusi.Name = "minusi";
+            this.minusi.Size = new System.Drawing.Size(100, 100);
+            this.minusi.TabIndex = 0;
+            this.minusi.Text = "-";
+            this.minusi.UseVisualStyleBackColor = false;
+            // 
+            // gayofa
+            // 
+            this.gayofa.BackColor = System.Drawing.Color.White;
+            this.gayofa.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gayofa.ForeColor = System.Drawing.Color.Black;
+            this.gayofa.Location = new System.Drawing.Point(328, 10);
+            this.gayofa.Name = "gayofa";
+            this.gayofa.Size = new System.Drawing.Size(100, 100);
+            this.gayofa.TabIndex = 0;
+            this.gayofa.Text = "÷";
+            this.gayofa.UseVisualStyleBackColor = false;
+            // 
+            // gamotvla
+            // 
+            this.gamotvla.Font = new System.Drawing.Font("Jokerman", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gamotvla.Location = new System.Drawing.Point(12, 10);
+            this.gamotvla.Multiline = true;
+            this.gamotvla.Name = "gamotvla";
+            this.gamotvla.Size = new System.Drawing.Size(312, 100);
+            this.gamotvla.TabIndex = 1;
+            this.gamotvla.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 544);
+            this.Controls.Add(this.gamotvla);
+            this.Controls.Add(this.minusi);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.toloba);
+            this.Controls.Add(this.washla);
+            this.Controls.Add(this.pliusi);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.wertili);
+            this.Controls.Add(this.gayofa);
+            this.Controls.Add(this.gamravleba);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "კალკულატორი";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button wertili;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button washla;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.Button gamravleba;
+        private System.Windows.Forms.Button pliusi;
+        private System.Windows.Forms.Button toloba;
+        private System.Windows.Forms.Button minusi;
+        private System.Windows.Forms.Button gayofa;
+        private System.Windows.Forms.TextBox gamotvla;
+    }
+}
+
---
+++ calculator/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void n1_Click(object sender, EventArgs e)
+        {
+            if (gamotvla.Text=="0")
+            {
+                gamotvla.Text="1";
+            }
+            else
+            {
+                gamotvla.Text+="1";
+            }
+        }
+    }
+}
